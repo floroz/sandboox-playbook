54df2ed279e2a8945a87941fb0c767bea3b52d11
Daniele Tortora <daniele@tray.io>
2021-09-19 17:59:59 +0200

    code highlighting and jsx processing

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -3,6 +3,10 @@ import prettier from "prettier";
 import parser from "prettier/parser-babel";
 import { editor } from "monaco-editor";
 import { useRef } from "react";
+import "./code-editor.css";
+import "./syntax.css";
+import codeShift from "jscodeshift";
+import Highlighter from "monaco-jsx-highlighter";
 
 const defaultInitialValue = `const message = 'Hello World!'\n\nconsole.log(message)`;
 
@@ -28,29 +32,50 @@ const CodeEditor = ({
     });
 
     monacoEditor.getModel()?.updateOptions({ tabSize: 2 });
+
+    const highlighter = new Highlighter(
+      // @ts-expect-error
+      window.monaco,
+      codeShift,
+      monacoEditor
+    );
+
+    highlighter.highLightOnDidChangeModelContent(
+      () => {},
+      () => {},
+      undefined,
+      () => {}
+    );
   };
 
   const onFormatClick = () => {
     const value = codeEditorRef.current?.getValue() ?? "";
 
     // format
-    const formatted = prettier.format(value, {
-      parser: "babel",
-      plugins: [parser],
-      useTabs: false,
-      semi: true,
-      singleQuote: true,
-    });
+    const formatted = prettier
+      .format(value, {
+        parser: "babel",
+        plugins: [parser],
+        useTabs: false,
+        semi: true,
+        singleQuote: true,
+      })
+      //remove end of line from prettier
+      .replace(/\n$/, "");
 
     codeEditorRef.current?.setValue(formatted);
   };
 
   return (
-    <div>
-      <button onClick={onFormatClick}>Format</button>
+    <div className="editor-wrapper">
+      <button
+        className="button button-format is-primary is-small"
+        onClick={onFormatClick}
+      >
+        Format
+      </button>
       <MonacoEditor
         height={400}
-        width={400}
         language="javascript"
         theme="dark"
         options={{
