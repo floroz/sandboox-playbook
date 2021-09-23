133b6996d2d68518f7e5db1978641c1d527e958f
Daniele Tortora <daniele@tray.io>
2021-09-23 15:24:12 +0200

    memoize action creators

--- src/hooks/useAction/index.ts
+++ src/hooks/useAction/index.ts
@@ -1,8 +1,12 @@
+import { useMemo } from "react";
 import { useDispatch } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as actionCreators from "../../store/action-creators";
 
 export const useAction = () => {
   const dispatch = useDispatch();
-  return bindActionCreators(actionCreators, dispatch);
+  return useMemo(
+    () => bindActionCreators(actionCreators, dispatch),
+    [dispatch]
+  );
 };
